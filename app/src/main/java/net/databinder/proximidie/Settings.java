870cffc869d6a3b0e4ba3053d083166423ff20be
Nathan Hamblen <nathan@technically.us>
2014-06-22 20:42:40 -0400

    add bluetooth devices to settings

--- app/src/main/java/net/databinder/proximidie/Settings.java
+++ app/src/main/java/net/databinder/proximidie/Settings.java
@@ -1,10 +1,15 @@
 package net.databinder.proximidie;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.preference.CheckBoxPreference;
+import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +19,10 @@ import android.os.Build;
 
 import net.databinder.proximidie.R;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 public class Settings extends ActionBarActivity {
 
     @Override
@@ -21,8 +30,9 @@ public class Settings extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         if (savedInstanceState == null) {
+            TriggerSettingsFragment settings = new TriggerSettingsFragment();
             getFragmentManager().beginTransaction()
-                    .replace(R.id.container, new TriggerSettingsFragment())
+                    .replace(R.id.container, settings)
                     .commit();
         }
     }
@@ -39,7 +49,7 @@ public class Settings extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public static class TriggerSettingsFragment extends PreferenceFragment {
+    public class TriggerSettingsFragment extends PreferenceFragment {
 
         public TriggerSettingsFragment() {
         }
@@ -48,6 +58,24 @@ public class Settings extends ActionBarActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            addBluetoothDevices((PreferenceCategory) findPreference("pref_key_devices_cat"));
+        }
+
+        private void addBluetoothDevices(PreferenceCategory cat) {
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter == null) {
+                Log.e(Settings.class.getName(), "Unable to get bluetooth adapter");
+            } else {
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+                for (BluetoothDevice device : pairedDevices) {
+                    CheckBoxPreference pref = new CheckBoxPreference(getApplicationContext());
+                    pref.setKey("bluetooth_device_" + device.getAddress());
+                    pref.setTitle(device.getName());
+                    cat.addPreference(pref);
+                }
+            }
         }
     }
 }
